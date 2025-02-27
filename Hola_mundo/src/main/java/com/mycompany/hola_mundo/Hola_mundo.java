e131c8b6303b6785ced71c23eda1a8a265edcdf9
Luvi  Code <luvicodes@gmail.com>
2025-02-27 08:22:15 -0500

    Add Files / Local Storage

---
+++ Hola_mundo/src/main/java/com/mycompany/hola_mundo/Hola_mundo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/*
+1. Crea un programa que imprima "Hola,Mundo" en la consola.
+*/
+
+
+package com.mycompany.hola_mundo;
+
+
+public class Hola_mundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola,Mundo!");
+    }
+}
