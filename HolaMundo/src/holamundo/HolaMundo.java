93614771c3dc398d3402e9ab0e9d0441bc7ad76e
Luvi  Code <luvicodes@gmail.com>
2025-02-28 11:10:44 -0500

    Add files via upload

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,15 @@
+
+package holamundo;
+/**
+ *
+ * @author luvi
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+        
+    }
+    
+}
